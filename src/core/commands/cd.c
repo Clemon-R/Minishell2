0f6516e90d5b592366c4232d47200ee78a95ab00
Raphaël Goulmot <raphael.goulmot@epitech.eu>
2017-04-09 17:34:08 +0200

    UpDate cd debug

--- src/core/commands/cd.c
+++ src/core/commands/cd.c
@@ -5,12 +5,13 @@
 ** Login   <raphael.goulmot@epitech.net>
 ** 
 ** Started on  Sat Apr  1 04:53:41 2017 Raphaël Goulmot
-** Last update Sun Apr  9 17:13:29 2017 Raphaël Goulmot
+** Last update Sun Apr  9 17:33:33 2017 Raphaël Goulmot
 */
 
 #include "utils.h"
 #include "commands.h"
 #include <unistd.h>
+#include <stdlib.h>
 
 int	error_handling(char *str, int size, int state, char **args)
 {
@@ -23,6 +24,19 @@ int	error_handling(char *str, int size, int state, char **args)
   else if (size > 2)
     my_putstr_err("cd: Too many arguments.\n");
   return (!str || size > 2 || state == -1 ? 1 : 0);
+}
+
+void	set_var(char ***env, char *var, char *str)
+{
+  char	**commands;
+
+  commands = malloc(sizeof(char *) * 4);
+  commands[0] = "setenv";
+  commands[1] = var;
+  commands[2] = str;
+  commands[3] = 0;
+  my_setenv(env, commands);
+  free(commands);
 }
 
 int	my_cd(char ***env, char **args)
@@ -43,6 +57,9 @@ int	my_cd(char ***env, char **args)
     value = args[1];
   path = getcwd(buff, 4096);
   if (value && size <= 2)
-    state = chdir(value);
+    {
+      state = chdir(value);
+      set_var(env, "OLDPWD", path);
+    }
   return (error_handling(value, size, state, args));
 }
